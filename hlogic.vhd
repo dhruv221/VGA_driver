905785fcc9d5a4ab57eefa4505929e08139ebe8a
dhruv221 <62287269+dhruv221@users.noreply.github.com>
2023-05-06 19:54:41 +0530

    Update hlogic.vhd

--- hlogic.vhd
+++ hlogic.vhd
@@ -5,25 +5,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity hlogic is
   Port (hcounter : in std_logic_vector(9 downto 0);
-        h_703 : inout std_logic;
-        h_800 : inout std_logic;
         h_000 : inout std_logic;
         h_639 : inout std_logic;
-        hsync : out std_logic;
-        rst   : out std_logic);
+        h_703 : inout std_logic;
+        h_800 : inout std_logic);
 end hlogic;
 
 architecture Behavioral of hlogic is
     
 begin
-h_703 <= hcounter(9) and not(hcounter(8)) and hcounter(7) and not(hcounter(6)) 
-         and hcounter(5) and hcounter(4) and hcounter(3) and hcounter(2) 
-         and hcounter(1) and hcounter(0);
 
-h_800 <= hcounter(9) and hcounter(8) and not hcounter(7) and not(hcounter(6)) 
-         and hcounter(5) and not hcounter(4) and not hcounter(3) and not hcounter(2) 
-         and not hcounter(1) and not hcounter(0);
-         
 h_000 <= not hcounter(9) and not hcounter(8) and not hcounter(7) and not(hcounter(6)) 
          and not hcounter(5) and not hcounter(4) and not hcounter(3) and not hcounter(2) 
          and not hcounter(1) and not hcounter(0);
@@ -31,18 +22,13 @@ h_000 <= not hcounter(9) and not hcounter(8) and not hcounter(7) and not(hcounte
 h_639 <= hcounter(9) and not hcounter(8) and not hcounter(7) and hcounter(6) 
          and hcounter(5) and hcounter(4) and hcounter(3) and hcounter(2) 
          and hcounter(1) and hcounter(0);
+         
+h_703 <= hcounter(9) and not(hcounter(8)) and hcounter(7) and not(hcounter(6)) 
+         and hcounter(5) and hcounter(4) and hcounter(3) and hcounter(2) 
+         and hcounter(1) and hcounter(0);
 
-process(h_703, h_800)
-begin
-if (h_703 = '1') then
-    hsync <= '0';
-elsif (h_800 = '1') then
-    hsync <= '1';
-    rst <= '1';
-elsif (h_000 = '1') then
-    hsync <= '1';
-    rst <= '0';
-end if;
-end process;
+h_800 <= hcounter(9) and hcounter(8) and not hcounter(7) and not(hcounter(6)) 
+         and hcounter(5) and not hcounter(4) and not hcounter(3) and not hcounter(2) 
+         and not hcounter(1) and not hcounter(0);
 
 end Behavioral;

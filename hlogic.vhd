a1cdfe6a50cb4db4b77f7f084e15b2b23e1c665f
dhruv221 <62287269+dhruv221@users.noreply.github.com>
2023-05-09 05:28:50 +0530

    Update hlogic.vhd

--- hlogic.vhd
+++ hlogic.vhd
@@ -1,13 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 
 entity hlogic is
   Port (hcounter : in std_logic_vector(9 downto 0);
         h_000 : inout std_logic;
-        h_639 : inout std_logic;
-        h_703 : inout std_logic;
+        h_640 : inout std_logic;
+        h_704 : inout std_logic; --sync
         h_800 : inout std_logic);
 end hlogic;
 
@@ -19,16 +19,31 @@ h_000 <= not hcounter(9) and not hcounter(8) and not hcounter(7) and not(hcounte
          and not hcounter(5) and not hcounter(4) and not hcounter(3) and not hcounter(2) 
          and not hcounter(1) and not hcounter(0);
          
-h_639 <= hcounter(9) and not hcounter(8) and not hcounter(7) and hcounter(6) 
-         and hcounter(5) and hcounter(4) and hcounter(3) and hcounter(2) 
-         and hcounter(1) and hcounter(0);
+h_640 <= hcounter(9) and not hcounter(8) and hcounter(7) and not hcounter(6) 
+         and not hcounter(5) and not hcounter(4) and not hcounter(3) and not hcounter(2) 
+         and not hcounter(1) and not hcounter(0);
          
-h_703 <= hcounter(9) and not(hcounter(8)) and hcounter(7) and not(hcounter(6)) 
-         and hcounter(5) and hcounter(4) and hcounter(3) and hcounter(2) 
-         and hcounter(1) and hcounter(0);
+h_704 <= hcounter(9) and not(hcounter(8)) and hcounter(7) and (hcounter(6)) 
+         and not hcounter(5) and not hcounter(4) and not hcounter(3) and not hcounter(2) 
+         and not hcounter(1) and not hcounter(0);
 
 h_800 <= hcounter(9) and hcounter(8) and not hcounter(7) and not(hcounter(6)) 
          and hcounter(5) and not hcounter(4) and not hcounter(3) and not hcounter(2) 
          and not hcounter(1) and not hcounter(0);
+
+--process(hcounter)
+--begin
+--if (to_integer(unsigned(hcounter)) = 0) then
+--    h_000 <= '1';
+--elsif (to_integer(unsigned(hcounter)) = 640) then
+--    h_639 <= '1';
+--elsif (to_integer(unsigned(hcounter)) = 656) then
+--    h_655 <= '1';
+--elsif (to_integer(unsigned(hcounter)) = 752) then
+--    h_751 <= '1';
+--elsif (to_integer(unsigned(hcounter)) = 800) then
+--    h_800 <= '1';
+--end if;
+--end process;
 
 end Behavioral;
